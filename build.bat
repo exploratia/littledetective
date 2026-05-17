768a2f44b6f979dced00f9c748954273ab38de6a
Christian Adler <chris.adler@gmx.de>
2026-05-17 16:56:30 +0200

    add build bat to ignore

--- build.bat
+++
@@ -1,9 +0,0 @@
-c:
-cd \
-cd dev/littledetective
-
-call flutter build apk --split-per-abi
-
-REM echo copy to desktop
-
-REM xcopy .\build\app\outputs\flutter-apk\app-arm64-v8a-release.apk D:\OneDrive\Desktop /Y
