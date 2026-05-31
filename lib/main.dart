3dc5dd69f118f7fb81ece09dd7bb87679c14d8eb
Christian Adler <chris.adler@gmx.de>
2026-05-31 06:12:56 +0200

    Activate Edge-to-Edge-View Flutter 3.16+

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/app.dart';
 
 void main() {
+  // Activates Edge-to-Edge-View Flutter 3.16+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent, systemNavigationBarColor: Colors.transparent));
   runApp(const LittleDetectiveApp());
 }
